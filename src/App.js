125fa8e78cd776ddb0663d5edd9daede62bf733f
Peter Bachman <57814978+PeterBachman100@users.noreply.github.com>
2025-05-07 16:06:46 -0700

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import Button from "./Button";
+
+function App() {
+    return (
+        <div>
+            <div>
+                <Button>Style 1</Button>
+            </div>
+            <div>
+                <Button>Style 2</Button>
+            </div>
+            <div>
+                <Button>Style 3</Button>
+            </div>
+            <div>
+                <Button>Style 4</Button>
+            </div>
+            <div>
+                <Button>Style 5</Button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
