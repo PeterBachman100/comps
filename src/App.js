627f6cdad66cbe20db4bacac261feabd0d429718
Peter Bachman <57814978+PeterBachman100@users.noreply.github.com>
2025-05-16 14:42:32 -0700

    Refactor: created DropdownPage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import { useState } from "react";
-import Dropdown from "./components/Dropdown";
+
 
 function App() {
-    const [selection, setSelection] = useState(null);
-
-    const handleSelect = (option) => {
-        setSelection(option);
-    }
-
-    const options = [
-        { label: 'Red', value: 'red'},
-        { label: 'Blue', value: 'blue'},
-        { label: 'Green', value: 'green'},
-    ];
+   
 
     return (
-        <div className="flex">
-            <Dropdown value={selection} onChange={handleSelect} options={options} />
+        <div>
         </div>
     );
 }
