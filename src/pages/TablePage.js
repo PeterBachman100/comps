6818c1ebc2c9f7e466b45827bfb0e92726cb113a
Peter Bachman <57814978+PeterBachman100@users.noreply.github.com>
2025-05-19 14:48:16 -0700

    Feat: Table Component

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -8,8 +8,18 @@ function TablePage() {
         {name: 'Lime', color: 'bg-green-500', score: 4},
     ];
 
+    const config = [
+        {label: 'Name', render: (fruit) => fruit.name },
+        {label: 'Color', render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>},
+        {label: 'Score', render: (fruit) => fruit.score }
+    ];
+
+    const keyFn = (fruit) => {
+        return fruit.name;
+    };
+
     return (
-        <Table data={data} />
+        <Table data={data} config={config} keyFn={keyFn}/>
     );
 }
 
